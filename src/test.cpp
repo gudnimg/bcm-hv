18ce53d6167ba8cfaf25148b519b9b8128948d4f
Guðni Már Gilbert <gudni.m.g@gmail.com>
2020-11-23 21:02:50 +0000

    Delete test.cpp

--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-for (int row = 0; row < N; row++) {
-    for (int col = 0; col < N; col++) {
-        int r = 0;
-        for (int k = 0; k < N; k++) {
-            r += a[row][k] * b[k][col];
-        }
-        x[row][j] = r;
-    }
-}
